97421881b41efc4195758bb0075c458994b93812
Liam McMains <lmcmains@codeauthority.com>
2019-06-14 16:48:33 -0500

    Add some comments and move some files around

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,11 +2,14 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import Navbar from './Navbar'
-import './all.sass'
+import '../resources/styles/all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
+    {/* Helmet is used for SEO, adds meta tags to your website and is configurable and dynamic */}
     <Helmet title="Home | Gatsby + WordPress" />
+
+    {/* Add the navbar to the top of the page and then render the child content */}
     <Navbar />
     <div>{children}</div>
   </div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -6,20 +6,25 @@ import { default as Image } from "gatsby-image"
 
 class HomePage extends Component {
     render() {
+        // Using optional chaining, if any of these properties are undefined then it will return undefined instead of giving an error
+        // eslint-disable-next-line
         let image = this.props?.data?.wordpressPage?.acf?.image?.localFile?.childImageSharp?.fixed;
 
+        // Create a styled title tag
         const Title = styled.h1`
             font-size: 20px;
             font-weight: bold;
             margin-bottom: 20px;
         `;
 
+        // Create a styled container tag
         const PaddedContainer = styled.div`
             padding: 50px 0px;
             max-width: 1000px;
             margin: 0px auto;
         `;
 
+        // Render the page with some text and image being pulled from advanced custom fields using the graphQL query at the bottom of the file
         return (
             <Layout>
                 <PaddedContainer>
@@ -31,6 +36,7 @@ class HomePage extends Component {
     }
 }
 
+// This query is responsible for pulling data about a page using the pageID that wordpress assigns it
 export const query = graphql`
 query currentPageQuery {
     wordpressPage(wordpress_id: {eq: 7}) {
