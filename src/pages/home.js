901fd364376e9dcdbbcb1335f8815428c5247356
Liam McMains <lmcmains@codeauthority.com>
2019-06-14 15:18:20 -0500

    Query images from ACF and have gatsby automatically handle them

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,43 +2,56 @@ import React, { Component } from 'react'
 import Layout from '../components/Layout'
 import { graphql } from 'gatsby';
 import styled from 'styled-components'
+import { default as Image } from "gatsby-image"
 
 class HomePage extends Component {
     render() {
-        const Image = styled.img`
-            width: 100px;
-            height: 100px;
-        `;
+        let image = this.props?.data?.wordpressPage?.acf?.image?.localFile?.childImageSharp?.fixed;
 
         const Title = styled.h1`
             font-size: 20px;
             font-weight: bold;
+            margin-bottom: 20px;
+        `;
+
+        const PaddedContainer = styled.div`
+            padding: 50px 0px;
+            max-width: 1000px;
+            margin: 0px auto;
         `;
 
         return (
             <Layout>
-                <Title> Test String: {this.props.data?.wordpressPage?.acf?.test_string } </Title>
-                <Image src={this.props.data?.wordpressPage?.acf?.image?.source_url }></Image>
+                <PaddedContainer>
+                    <Title> Test String: {this.props.data?.wordpressPage?.acf?.test_string } </Title>
+                    <Image fixed={image} />
+                </PaddedContainer>
             </Layout>
         );
     }
 }
 
 export const query = graphql`
-    query currentPageQuery {
-        wordpressPage(wordpress_id: {eq: 7}) {
-        title
-        content
-        slug
-        date(formatString: "MMMM DD, YYYY")
-        acf {
-            test_string
-            image {
-            source_url
+query currentPageQuery {
+    wordpressPage(wordpress_id: {eq: 7}) {
+      title
+      content
+      slug
+      date(formatString: "MMMM DD, YYYY")
+      acf {
+        test_string
+        image {
+          localFile {
+            childImageSharp {
+              fixed(width: 300, height: 300) {
+                ...GatsbyImageSharpFixed
+              }
             }
+          }
         }
-        }
+      }
     }
+  }  
 `;
 
 export default HomePage;
